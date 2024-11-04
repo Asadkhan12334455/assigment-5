a04c617ca7f01c2dd8db803406d3b3de4ec463c2
Asadkhan12334455 <asada1l2i3z4a5@gmail.com>
2024-11-04 22:58:39 +0500

    figma Style

--- src/pages/components/Header.tsx
+++ src/pages/components/Header.tsx
@@ -3,10 +3,7 @@ import React from 'react';
 const Header = () => {
   return (
     <div className="h-[88px] bg-[#A29875] flex items-center px-6">
-      <h1 
-        className="text-[44px] text-white" 
-        style={{ fontFamily: 'Libre Bodoni' }} // Corrected inline style syntax
-      >
+      <h1 className="text-[44px] font-rye text-white">
         MANZZARI
       </h1>
     </div>
